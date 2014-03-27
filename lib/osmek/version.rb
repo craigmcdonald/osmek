77a3d8cc660154d518ecf75b690987387f662543
craigmcdonald <craig@storkup.com>
2014-03-27 17:30:26 +0000

    Update version.rb

--- lib/osmek/version.rb
+++ lib/osmek/version.rb
@@ -1,17 +1,3 @@
 module Osmek
-  class Version
-    MAJOR = 0 unless defined? Osmek::MAJOR
-    MINOR = 0 unless defined? Osmek::MINOR
-    PATCH = 1 unless defined? Osmek::PATCH
-    PRE = nil unless defined? Osmek::PRE
-
-    class << self
-
-      def to_s
-        [MAJOR, MINOR, PATCH, PRE].compact.join('.')
-      end
-
-    end
-
-  end
+  VERSION = "0.0.2"
 end
